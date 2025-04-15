4ae2f388ae85dedf2efe39b0870988fd15696409
Loh Chun Mun <102757707+cmsamaaa@users.noreply.github.com>
2025-04-16 01:08:44 +0800

    Add staff email template and enhance email handling in Lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -12,26 +12,50 @@ def read_email_template():
   with open('email.html', 'r') as file:
     return file.read()
 
+def read_staff_email_template():
+  with open('staff_email.html', 'r') as file:
+    return file.read()
 
 def lambda_handler(event, context):
   for record in event['Records']:
     message_body = record['body']
     print(f"Received message: {message_body}")
 
+    personalized_email: str = ''
+
     try:
       data = json.loads(message_body)
 
-      # Read email template
-      email_template = read_email_template()
+      if data['role'] == "BUSINESS_OWNER":
+        print("Sending email for BUSINESS_OWNER...")
 
-      # Replace placeholders with actual data
-      personalized_email = (
-        email_template
-        .replace('{{CompanyName}}', "QueueTopia")
-        .replace('{{Name}}', f"{data['first_name']} {data['last_name']}")
-        .replace('{{Email}}', data['email'])
-        .replace('{{ConfirmationUrl}}', "https://queuetopia-ui.vercel.app/confirm?email=" + data['email'])
-      )
+        # Read email template
+        email_template = read_email_template()
+
+        # Replace placeholders with actual data
+        personalized_email = (
+          email_template
+          .replace('{{CompanyName}}', "QueueTopia")
+          .replace('{{Name}}', f"{data['first_name']} {data['last_name']}")
+          .replace('{{Email}}', data['email'])
+          .replace('{{ConfirmationUrl}}', "https://queuetopia-ui.vercel.app/confirm?email=" + data['email'])
+        )
+
+      if data['role'] == "EMPLOYEE":
+        print("Sending email for EMPLOYEE...")
+
+        # Read staff email template
+        email_template = read_staff_email_template()
+
+        # Replace placeholders with actual data
+        personalized_email = (
+          email_template
+          .replace('{{CompanyName}}', "QueueTopia")
+          .replace('{{Name}}', f"{data['first_name']} {data['last_name']}")
+          .replace('{{Email}}', data['email'])
+          .replace('{{Password}}', data['password'])
+          .replace('{{ConfirmationUrl}}', "https://queuetopia-ui.vercel.app/confirm?email=" + data['email'])
+        )
 
       params: resend.Emails.SendParams = {
         "from": "QueueTopia <no-reply@notification.webdevsolutions.io>",
